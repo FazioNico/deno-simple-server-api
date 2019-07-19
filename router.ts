f7b7a5cd50dde1f764059a4267a5ecf3d2888a06
FazioNico <contact@nicolasfazio.ch>
2019-07-19 15:20:54 +0200

    fix - handle 404

--- router.ts
+++ router.ts
@@ -5,6 +5,10 @@ import { ServerRequest, Response } from 'https://deno.land/std/http/server.ts';
 export const router = async (req: ServerRequest): Promise<void> => {
   let data: Response = {};
   switch (true) {
+    // handle default request
+    case req.url === '/':
+      data.body =  new TextEncoder().encode('Hello World\n');
+      break;
     case req.url.includes('reqres'):
       // extract params url
       const [baseUrl, ...params] = req.url.split('/reqres/');
@@ -13,12 +17,10 @@ export const router = async (req: ServerRequest): Promise<void> => {
       // do request to https://reqres.in with correct params
       data = await await fetch(url, {}).then(res => ({...res})).catch(err => err)
       break;
-    // handle default request
     default:
-      data.body =  new TextEncoder().encode('Hello World\n');
       break;
   }
-  if (!data.body) return req.respond({status: 404, body: new TextEncoder().encode('Error response: Server endpoint not found.') });
+  if (!data.body) return req.respond({status: 404, body: new TextEncoder().encode('Error 404: Server endpoint not found.') });
   // return response
   return await req.respond(data).catch(err => err);
 }
